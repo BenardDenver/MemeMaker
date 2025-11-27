ba46513af5ae9894078e8638debdf71dda135d59
BNRD_10k Bishop <chiherengebenard10@gmail.com>
2025-11-27 10:47:57 +0200

    Initial Commit

---
+++ MemeMaker/MemeMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemeMakerApp.swift
+//  MemeMaker
+//
+//  Created by Benard Chiherenge on 27/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct MemeMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
